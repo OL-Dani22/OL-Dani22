9f0f40224f7c44a0d293d7e7908999de73da54da
Dani <56628625+oldy-22@users.noreply.github.com>
2023-12-03 21:06:20 +0100

    Add files via upload

--- csv2bibtex/CsvToBibTeXConverter.java
+++ csv2bibtex/CsvToBibTeXConverter.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class CsvToBibTeXConverter {
 
     public static void main(String[] args) {
-        String csvFile = "deine_datei.csv"; // Passe den Dateinamen an
-        String bibtexFile = "ergebnis.bib"; // Passe den Dateinamen an
+        String csvFile = "dump.csv"; // Passe den Dateinamen an
+        String bibtexFile = "lit.bib"; // Passe den Dateinamen an
 
         try {
             convertCsvToBibTeX(csvFile, bibtexFile);
@@ -23,19 +23,34 @@ public class CsvToBibTeXConverter {
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile));
              FileWriter fw = new FileWriter(bibtexFile)) {
 
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] fields = line.split(",");
+            String line= br.readLine(); // 1. Zeile fällt weg
+           while ((line = br.readLine()) != null) {
+                String[] fields = line.split("\",\"");
 
-                // Annahme: Die erste Spalte enthält den BibTeX-Schlüssel
-                String bibtexKey = fields[0].trim();
+                // führendes " löschen & Key erzeugen
+                String groups = fields[0].substring(1);
+                String year;
+                if (fields[5].startsWith("9")) year = "19"+fields[5];
+                	else year = "20"+fields[5];
+                String bibtexKey = fields[2].substring(0, fields[2].indexOf(' '));
+                bibtexKey = bibtexKey.substring(0, bibtexKey.length()-1);
+                bibtexKey = bibtexKey + year;
 
                 // Aufbau der BibTeX-Einträge (angepasst an deine CSV-Struktur)
-                String bibtexEntry = "@article{" + bibtexKey + ",\n";
-                bibtexEntry += "  author = {" + fields[1] + "},\n";
-                bibtexEntry += "  title = {" + fields[2] + "},\n";
-                bibtexEntry += "  journal = {" + fields[3] + "},\n";
-                bibtexEntry += "  year = {" + fields[4] + "},\n";
+                String bibtexEntry = "@book{" + bibtexKey + ",\n";
+                bibtexEntry += "  groups = {" + groups + "},\n";
+                bibtexEntry += "  author = {" + fields[2] + "},\n";
+                bibtexEntry += "  title = {" + fields[3] + "},\n";
+                bibtexEntry += "  date = {" + year + "-0" + fields[4] + "-01},\n";
+                bibtexEntry += "  priority = {" + fields[1] + "},\n";
+                bibtexEntry += "  owner = {" + fields[6] + "},\n";
+                bibtexEntry += "  number = {" + fields[7] + "},\n";
+                bibtexEntry += "  edition = {" + fields[8] + "},\n";
+                bibtexEntry += "  publisher = {" + fields[9] + "},\n";
+                bibtexEntry += "  number = {" + fields[10] + "},\n";
+                bibtexEntry += "  year = {" + fields[11] + "},\n";
+                bibtexEntry += "  isbn = {" + fields[12] + "},\n";
+                bibtexEntry += "  abstract = {" + fields[13] + "},\n";
                 // Füge weitere Felder nach Bedarf hinzu
 
                 bibtexEntry += "}\n\n";
